33147d568a6c7f18ddfeba1007738db8afa013f7
Nate Roundy <nroundy801@gmail.com>
2019-11-01 16:50:36 -0600

    wokring on requesting data from the backend

--- server/controller.js
+++ server/controller.js
@@ -6,8 +6,16 @@ module.exports = {
   },
 
   read: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+    // const {image_url, price, name} = req.body;
 
-    res.status(200).send("It worked!!! Woohoo!!!")
+    dbInstance.get_inventory([ shelfie ])
+      .then( () => res.sendStatus(200) )
+      .catch( err => {
+        res.status(500).send({errorMessage: "Oops! Something went wrong. Our engineers have been informed!"});
+        console.log(err)
+      });
+    // res.status(200).send("It worked!!! Woohoo!!!")
   },
 
   update: (req, res, next) => {
